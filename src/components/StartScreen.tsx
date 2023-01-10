5c66a7a16619deafa06e69cbf0d8aca672e69b30
glocore <mailtoashwinm@gmail.com>
2023-01-10 17:09:51 +0530

    add start screen

---
+++ src/components/StartScreen.tsx
@@ -0,0 +1,13 @@
+import "./StartScreen.css";
+
+export function StartScreen() {
+  return (
+    <div className="start-screen">
+      <div className="panel">
+        <span>Do you want to play a game?</span>
+        <br />
+        <button>Begin</button>
+      </div>
+    </div>
+  );
+}
